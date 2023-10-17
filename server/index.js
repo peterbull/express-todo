4817d3506de2793bd75abdf64f55000268392270
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-10-17 14:29:13 -0400

    move files to server folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
     console.log("server has started listening on port 5000")
