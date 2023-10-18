2b9962ae451877b26d681102d1786d785a097ece
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-10-18 17:01:09 -0400

    add get by id

--- server/index.js
+++ server/index.js
@@ -13,40 +13,46 @@ app.use(express.json()); //req.body
 // create a todo
 
 app.post("/todos", async (req, res) => {
-    try {
-        const { description } = req.body;
-        const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1) RETURNING *",
-            [description]
-        );
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
 
-        res.json(newTodo.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 // get all todos
 app.get("/todos", async (req, res) => {
-    try {
-        const allTodos = await pool.query(
-            "SELECT * FROM todo"
-        );
+  try {
+    const allTodos = await pool.query(
+      "SELECT * FROM todo"
+    );
 
-        res.json(allTodos.rows)
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message)
+  }
+});
 
 // get a todo
 app.get("/todos/:id", async (req, res) => {
-    try {
-        console.log(req.params)
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+  try {
+    const { id } = req.params
+    const todo = await pool.query(
+      "SELECT * FROM todo WHERE todo_id = $1", 
+      [id]
+    );
+		
+		res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // update a todo
 
