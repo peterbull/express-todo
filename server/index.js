eb526c017893eaf0ec4f51dbb7b3c0b690b4d5c9
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-10-18 05:45:44 -0400

    add db connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db")
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+// routes
+
 
 app.listen(5000, () => {
     console.log("server has started listening on port 5000")
